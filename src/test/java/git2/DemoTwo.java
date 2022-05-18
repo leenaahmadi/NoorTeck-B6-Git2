f36cf2ffa87cc9d838c166aeebe430700e163626
leenaahmadi <leenaahmadi@leenas-mbp.lan>
2022-05-17 23:07:05 -0700

    Implemented add method

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -5,5 +5,9 @@ public class DemoTwo {
 	public static void main(String[] args) {
 		System.out.println("Git demo 2...");
 	}
+	
+	public void add(int a , int b) {
+		System.out.println(a+b);
+	}
 
 }
