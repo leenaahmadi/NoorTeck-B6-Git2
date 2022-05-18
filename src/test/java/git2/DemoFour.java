3b14b712e3a760db8f177548723da81cccf82254
leenaahmadi <leenaahmadi@leenas-mbp.lan>
2022-05-17 23:39:58 -0700

    Stash practice....

--- src/test/java/git2/DemoFour.java
+++ src/test/java/git2/DemoFour.java
@@ -5,4 +5,9 @@ public class DemoFour {
 	public static void main(String[] args) {
 		System.out.println("Demo Four...");
 	}
+	
+	public static void div(int a, int b) {
+		int result = a/b;
+		System.out.println(result);
+	}
 }
